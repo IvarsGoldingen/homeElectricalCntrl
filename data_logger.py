3a8ca4ef7852b58fe15cf271b0160e7375a1edac
IvarsGoldingen <briedisivars@gmail.com>
2023-12-03 16:59:56 +0200

    Implemented periodical logging

--- data_logger.py
+++ data_logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from enum import Enum, auto
+from threading import Timer
 import queue
 import time
 import threading
@@ -16,11 +17,11 @@ from database_mngr import DbMngr
 # Setup logging
 log_formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 # Console debug
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(log_formatter)
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 # File logger
 file_handler = logging.FileHandler(os.path.join("logs", "data_logger.log"))
@@ -37,7 +38,6 @@ class DataLogger(Observer):
     """
     Class for logging historical data of home automation program
     Periodical and on data change logging
-    TODO: implement periodical logging
     """
 
     class LogType(Enum):
@@ -45,12 +45,31 @@ class DataLogger(Observer):
         SHELLY_LOG = auto()
         STOP_LOG = auto()
 
-    def __init__(self, get_prices_method: Callable[[], Tuple[Dict, Dict]], device_list: list[Device]):
+    def __init__(self, get_prices_method: Callable[[], Tuple[Dict, Dict]], device_list: list[Device],
+                 device_log_interval_s: float = 3600.0):
+        """
+        :param get_prices_method: Method to call for this class to get the prices of electricitt
+        :param device_list: list of devices hwose data is to be logged
+        :param device_log_interval_s: how often to periodically log device data
+        """
+        self.device_log_interval_s = device_log_interval_s
         self.get_prices_method = get_prices_method
         self.device_list = device_list
         self.data_queue = queue.Queue()
+        self.periodical_log_thread = Timer(self.device_log_interval_s, self.periodical_device_log)
+        self.periodical_log_thread.start()
         self.db_thread = threading.Thread(target=self.db_mngr_thread_method, args=(self.data_queue,))
         self.db_thread.start()
+
+    def periodical_device_log(self):
+        logger.debug("periodical_device_log")
+        # Execute this function in regular intervals
+        for dev in self.device_list:
+            if dev.get_cmd_given():
+                logger.debug(f"Device {dev.name} is on, executing periodical log")
+                self.log_device_data(dev)
+        self.periodical_log_thread = Timer(self.device_log_interval_s, self.periodical_device_log)
+        self.periodical_log_thread.start()
 
     def handle_subject_event(self, event_type: str, *args, **kwargs):
         # Handle events initiated by devices this class is listening to
@@ -64,18 +83,21 @@ class DataLogger(Observer):
                 for key, value in kwargs.items():
                     logger.error(f"{key}: {value}")
                 return
-            self.handle_device_event(device_type, device_name)
+            self.handle_device_event(device_name)
 
-    def handle_device_event(self, device_type: DeviceType, device_name: str):
+    def handle_device_event(self, device_name: str):
         # Log data for different devices
-        logger.debug(f"Handling device event type {device_type}, name {device_name}")
+        logger.debug(f"Handling device event name {device_name}")
         dev_to_log = self.get_device_by_name(device_name)
         if not dev_to_log:
             logger.error("Device with specific name not in device list")
             return
+        self.log_device_data(dev_to_log)
+
+    def log_device_data(self, dev: Device):
+        device_type = dev.device_type
         if device_type == DeviceType.SHELLY_PLUG:
-            self.data_queue.put({"log_type": self.LogType.SHELLY_LOG, "data": dev_to_log})
-            # self.log_shelly_data(dev_to_log)
+            self.data_queue.put({"log_type": self.LogType.SHELLY_LOG, "data": dev})
         else:
             logger.error(f"Device type not recognised {device_type}")
 
@@ -95,6 +117,9 @@ class DataLogger(Observer):
         self.data_queue.put({"log_type": self.LogType.PRICE_LOG, "data": (prices_tomorrow, tomorrows_date)})
 
     def stop(self):
+        # Stop periodicall logging
+        self.periodical_log_thread.cancel()
+        # Stop database thread
         self.data_queue.put({"log_type": self.LogType.STOP_LOG})
 
     def db_mngr_thread_method(self, data_queue):
