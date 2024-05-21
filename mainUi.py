475c66b9515ad58c1bb7a96e1d17edc53e648924
IvarsGoldingen <briedisivars@gmail.com>
2024-05-21 19:38:02 +0300

    Settings change

--- mainUi.py
+++ mainUi.py
@@ -163,11 +163,11 @@ class MainUIClass(Tk, Observer):
 
     def setup_schedules(self):
         self.schedule_2days = HourlySchedule2days("2 DAY SCHEDULE")
-        # self.schedule_2days.add_to_device_list(self.plug1)
+        self.schedule_2days.add_to_device_list(self.plug1)
         self.auto_sch_creator = AutoScheduleCreator(get_prices_method=self.price_mngr.get_prices_today_tomorrow,
                                                     hourly_schedule=self.schedule_2days)
         self.alarm_clock = DailyTimedSchedule(name="Alarm clock", hour_on=7, minute_on=00, on_time_min=15)
-        self.alarm_clock.add_device(self.plug1)
+        self.alarm_clock.add_device(self.smart_relay1)
         self.christmas_lights = DailyTimedSchedule(name="Christmas lights", hour_on=18, minute_on=00, on_time_min=240)
         self.christmas_lights.add_device(self.plug2)
 
